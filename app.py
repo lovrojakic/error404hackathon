2ce9fa5d3eb298d2dd861f0f24255782f6784c84
lovrojakic <lovrojakic@users.noreply.github.com>
2021-03-25 19:18:58 +0100

    a

--- app.py
+++ app.py
@@ -4,3 +4,19 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return render_template('index.html')
+
+@app.route("/index-1.html")
+def i1():
+    return render_template('index-1.html')
+
+@app.route("/index-2.html")
+def i2():
+    return render_template('index-2.html')
+
+@app.route("/index-3.html")
+def i3():
+    return render_template('index-3.html')
+
+@app.route("/index-4.html")
+def i4():
+    return render_template('index-4.html')
