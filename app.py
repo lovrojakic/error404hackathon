d757e39299a04f8742c1c1bab8926848977b267f
lovrojakic <lovrojakic@users.noreply.github.com>
2021-03-25 21:44:49 +0100

    a

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify, Response
+from amadeus import Client, ResponseError
+import requests
 app = Flask(__name__)
+
+amadeus = Client(
+    client_id='CujnhbJ1hfGIco8AkJ6DktAzK1KW0ARp',
+    client_secret='7OotVvuBVqcMmONt'
+)
 
 @app.route("/")
 def hello():
@@ -24,3 +31,7 @@ def i3():
 @app.route("/index-4.html")
 def i4():
     return render_template('index-4.html')
+
+@app.route("/json-test")
+def jsontest():
+    return (amadeus.reference_data.urls.checkin_links.get(airlineCode='BA')).result
